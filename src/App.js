2634d1527637174fa76e0b387fc09cb11c538730
qhen143 <hengquentin@gmail.com>
2023-02-05 14:12:37 +1300

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 
+function HalfSquare(props) {
+  return <button className="halfSquare">{props.value}</button>
+}
+
+function Square(props) {
+  return <button className="square">{props.value}</button>
+}
+
+function Shop(props) {
+  return (
+    <div className="shop">
+      <Square value={props.value} />
+      <Square value={props.value} />
+      <Square value={props.value} />
+      <Square value={props.value} />
+      <Square value={props.value} />
+    </div>
+  )
+}
+
+function ActionBar(props) {
+  return (
+    <div className="row">
+      <div className="actions">
+        <HalfSquare value={props.value} />
+        <HalfSquare value={props.value} />
+        </div>
+      <Shop value={props.value} />      
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
@@ -9,6 +41,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Square value="1" />
+        <Shop value="1" />
+        <p></p>
+        <ActionBar value="1" />
         <a
           className="App-link"
           href="https://reactjs.org"
